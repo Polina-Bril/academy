82b6d7b0bd9be1e4893da16bc8871d822bc0be66
Alexey <alexey.kovzel@gmail.com>
2020-06-29 19:33:54 +0300

    First commit

---
+++ academy/src/by/academy/classwork/Main.java
@@ -0,0 +1,15 @@
+package by.academy.classwork;
+
+public class Main {
+	public static void main(String[] args) {
+		int myAge = 24;
+		long passportNumber = 1234567890L;
+		char yourMark = 'A';
+		double averageMark = 4.12;
+		short test = 512;
+				
+		System.out.println("your exam results: ");
+		System.out.println("your age is: "+myAge+", passport number: "+passportNumber);
+		System.out.println("you got "+yourMark+", average mark is "+averageMark);
+	}
+}
