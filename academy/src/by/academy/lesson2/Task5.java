9b04e3f5f53def86aafb660798eb1095f22fd519
Polina Bril <venera_48@mail.ru>
2020-06-30 23:29:18 +0300

    Task3-5

---
+++ academy/src/by/academy/lesson2/Task5.java
@@ -0,0 +1,26 @@
+package by.academy.lesson2;
+
+import java.util.Scanner;
+
+public class Task5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("¬ведите первое число");
+		double a = sc.nextDouble();
+		System.out.println("¬ведите второе число");
+		double b = sc.nextDouble();
+		double c = (a+b)/2;
+		if (a>b) {
+			System.out.println(a+" - наибольшее, "+b+" - наименьшее");
+		} else if(a==b) {
+			System.out.println("„исла равны");
+				} else 	{
+					System.out.println(a+" - наименьшее, "+b+" - наибольшее");
+					}
+		System.out.println(c+" - среднее арифметическое");
+		sc.close();
+	}
+
+}
