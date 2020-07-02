41041d31eba34f4462b4ed7a2bc166b61539d89b
Polina Bril <venera_48@mail.ru>
2020-07-03 02:16:24 +0300

    arr1-2

---
+++ academy/src/by/academy/lesson3/Massive1.java
@@ -0,0 +1,21 @@
+package by.academy.lesson3;
+
+public class Massive1 {
+
+	public static void main(String[] args) {
+		int even [] = new int [10]; 
+		int j = 0;
+		for(int i=2;i<=20;i++) {
+			if (i%2==0) {
+				even[j]=i;
+				System.out.print(even[j]+" ");
+				j++;
+		}
+	}
+		System.out.println(" ");
+		for(int i=0;i<10;i++) {
+			System.out.println(even[i]);
+						}
+	}
+}
+
--- academy/src/by/academy/lesson3/Massive2.java
+++ academy/src/by/academy/lesson3/Massive2.java
@@ -3,8 +3,20 @@ package by.academy.lesson3;
 public class Massive2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int arr[]=new int[50];
+		int j=0;
+		for(int i=1;i<=99;i++) {
+			if(i%2==1) {
+				arr[j]=i;
+				System.out.print(arr[j]+" ");
+				j++;
+			}
+		}
+		System.out.println("");
+		for(int i=50-1;i>=0;i--) {
+			System.out.print(arr[i]+" ");
 	}
-
+	}
 }
+
+
