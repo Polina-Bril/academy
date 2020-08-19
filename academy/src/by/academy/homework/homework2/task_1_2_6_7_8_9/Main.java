bc59e7bf5300713879a95dd1c4ef6f4c4ffbd8e8
Polina Bril <venera_48@mail.ru>
2020-08-19 21:08:40 +0300

    :(

--- academy/src/by/academy/homework/homework2/task_1_2_6_7_8_9/Main.java
+++ academy/src/by/academy/homework/homework2/task_1_2_6_7_8_9/Main.java
@@ -2,7 +2,7 @@ package by.academy.homework.homework2.task_1_2_6_7_8_9;
 
 import java.util.Scanner;
 
-import by.academy.homework2.dealRegExpStroki.Task3;
+import by.academy.homework.homework2.Task3;
 
 public class Main {
 
@@ -12,80 +12,80 @@ public class Main {
 		User seller = new User();
 		String s1 = sc.nextLine();
 		seller.setName(s1);
-
-		do {
-			System.out.println("Введите дату рождения продавца  формате dd/MM/yyyy или dd-MM-yyyy");
-			String s3 = sc.nextLine();
-			boolean isValid = Task3.isValidDateString(s3);
-			seller.setDateOfBirth(s3);
-			if (isValid == true) {
-				break;
-			} else {
-				System.out.println("Дата введена неверно.");
-			}
-		} while (true);
-
-		do {
-			System.out.println("Введите телефон продавца");
-			String s5 = sc.nextLine();
-			boolean isValid = BelarusPhoneValidator.validate(s5);
-			seller.setPhone(s5);
-			if (isValid == true) {
-				break;
-			} else {
-				System.out.println("Телефон введен неверно.");
-			}
-		} while (true);
-		do {
-			System.out.println("Введите email продавца");
-			String s6 = sc.nextLine();
-			boolean isValid = EmailValidator.validate(s6);
-			seller.setEmail(s6);
-			if (isValid == true) {
-				break;
-			} else {
-				System.out.println("Email введен неверно.");
-			}
-		} while (true);
+		
+		 do {
+		 System.out.println("Введите дату рождения продавца формате dd/MM/yyyy или  dd-MM-yyyy");
+		 String s3 = sc.nextLine();
+		 boolean isValid = Task3.isValidDateString(s3);
+		 seller.setDateOfBirth(s3);
+		 if (isValid == true) {
+		 break;
+		 } else {
+		 System.out.println("Дата введена неверно.");
+		 }
+		 } while (true);
+		
+		 do {
+		 System.out.println("Введите телефон продавца");
+		 String s5 = sc.nextLine();
+		 boolean isValid = BelarusPhoneValidator.validate(s5);
+		 seller.setPhone(s5);
+		 if (isValid == true) {
+		 break;
+		 } else {
+		 System.out.println("Телефон введен неверно.");
+		 }
+		 } while (true);
+		 do {
+		 System.out.println("Введите email продавца");
+		 String s6 = sc.nextLine();
+		 boolean isValid = EmailValidator.validate(s6);
+		 seller.setEmail(s6);
+		 if (isValid == true) {
+		 break;
+		 } else {
+		 System.out.println("Email введен неверно.");
+		 }
+		 } while (true);
 
 		User buyer = new User();
 		System.out.println("Введите имя покупателя");
 		String s2 = sc.nextLine();
 		buyer.setName(s2);
-
-		do {
-			System.out.println("Введите дату рождения покупателя в  формате dd/MM/yyyy или dd-MM-yyyy");
-			String s4 = sc.nextLine();
-			boolean isValid = Task3.isValidDateString(s4);
-			buyer.setDateOfBirth(s4);
-			if (isValid == true) {
-				break;
-			} else {
-				System.out.println("Дата введена неверно.");
-			}
-		} while (true);
-		do {
-			System.out.println("Введите телефон покупателя");
-			String s7 = sc.nextLine();
-			boolean isValid = BelarusPhoneValidator.validate(s7);
-			buyer.setPhone(s7);
-			if (isValid == true) {
-				break;
-			} else {
-				System.out.println("Телефон введен неверно.");
-			}
-		} while (true);
-		do {
-			System.out.println("Введите email покупателя");
-			String s8 = sc.nextLine();
-			boolean isValid = EmailValidator.validate(s8);
-			buyer.setEmail(s8);
-			if (isValid == true) {
-				break;
-			} else {
-				System.out.println("Email введен неверно.");
-			}
-		} while (true);
+		
+		 do {
+		 System.out.println("Введите дату рождения покупателя в формате dd/MM/yyyy или dd-MM-yyyy");
+		 String s4 = sc.nextLine();
+		 boolean isValid = Task3.isValidDateString(s4);
+		 buyer.setDateOfBirth(s4);
+		 if (isValid == true) {
+		 break;
+		 } else {
+		 System.out.println("Дата введена неверно.");
+		 }
+		 } while (true);
+		 do {
+		 System.out.println("Введите телефон покупателя");
+		 String s7 = sc.nextLine();
+		 boolean isValid = BelarusPhoneValidator.validate(s7);
+		 buyer.setPhone(s7);
+		 if (isValid == true) {
+		 break;
+		 } else {
+		 System.out.println("Телефон введен неверно.");
+		 }
+		 } while (true);
+		 do {
+		 System.out.println("Введите email покупателя");
+		 String s8 = sc.nextLine();
+		 boolean isValid = EmailValidator.validate(s8);
+		 buyer.setEmail(s8);
+		 if (isValid == true) {
+		 break;
+		 } else {
+		 System.out.println("Email введен неверно.");
+		 }
+		 } while (true);
 
 		System.out.println("Введите количество продуктов в данной сделке");
 		int n = sc.nextInt();
@@ -102,9 +102,9 @@ public class Main {
 
 			if (type.equals("продукт1")) {
 				products[i] = new Product1(productName, price, quantity, type);
-			}else if (type.equals("продукт2")) {
+			} else if (type.equals("продукт2")) {
 				products[i] = new Product2(productName, price, quantity, type);
-			}else if (type.equals("продукт3")) {
+			} else if (type.equals("продукт3")) {
 				products[i] = new Product3(productName, price, quantity, type);
 			}
 		}
---
+++ academy/src/by/academy/lesson14/Task1v12Files.java
@@ -0,0 +1,18 @@
+package by.academy.lesson14;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Task1v12Files {
+
+	// Переписать класс FileCopy:
+	// https://github.com/dmitry-academy/academy/blob/master/academy/src/by/academy/lesson12/bytestreams/FileCopy.java
+	// a) Добавить блок try-with-resources - создать класс с имплементацией closeble
+	// б) Добавить catch блок для обработки IOException.
+
+	public static void main(String... args) {
+
+	}
+
+}
