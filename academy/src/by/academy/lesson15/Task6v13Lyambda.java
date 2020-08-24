1ac0917fc6a46f426d7d42e4cc27ed122b7d1a34
Polina Bril <venera_48@mail.ru>
2020-08-24 21:16:42 +0300

    +

--- academy/src/by/academy/lesson15/Task6v13Lyambda.java
+++ academy/src/by/academy/lesson15/Task6v13Lyambda.java
@@ -1,9 +1,13 @@
 package by.academy.lesson15;
 
 public class Task6v13Lyambda {
+	
+//	Написать лямбда выражение, которое принимает на вход объект типа HeavyBox и 
+//	выводит на консоль сообщение “Отгрузили ящик с весом n”. “Отправляем ящик с весом n” 
+//	Используем функциональный интерфейс Consumer и метод по умолчанию andThen.
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
